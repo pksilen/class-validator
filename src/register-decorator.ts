7baec30c9337a67215e3da605e0d59fd6c6281d0
Petri Silen <petri.silen@silensoft.com>
2021-11-13 16:28:01 +0200

    Modified

--- src/register-decorator.ts
+++ src/register-decorator.ts
@@ -75,7 +75,7 @@ export function registerDecorator(options: ValidationDecoratorOptions): void {
   }
 
   const validationMetadataArgs: ValidationMetadataArgs = {
-    type: options.name && ValidationTypes.isValid(options.name) ? options.name : ValidationTypes.CUSTOM_VALIDATION,
+    type: options.name ??  ValidationTypes.CUSTOM_VALIDATION,
     target: options.target,
     propertyName: options.propertyName,
     validationOptions: options.options,
